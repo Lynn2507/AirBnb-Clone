530939c2ea422608fa417b551e3bb33b63f6a4e6
Lynn Yap <weilynny@gmail.com>
2024-02-27 15:45:04 +1030

    newly created files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    rules: {
+        'no-console': 'off',
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+
+    "env": {
+        "es6": true
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
+    }
+};
