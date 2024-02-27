a82fa05cd297478e623bab41600d6422de2c8b34
Lynn Yap <weilynny@gmail.com>
2024-02-28 09:34:51 +1030

    deleted files n folder

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import airBnb from './assets/Airbnb-logo.png'
+import './App.css'
+
+
+export default function App() {
+    return (
+        <div>
+            <nav>
+                <img src={airBnb}></img>
+            </nav>
+        </div>
+    )
+}
