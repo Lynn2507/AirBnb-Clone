ea94c085ab0690e47883fe6e4937c3720ccc7837
Lynn Yap <weilynny@gmail.com>
2024-02-29 09:53:56 +1030

    commit progress

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,28 +1,42 @@
-import airBnb from './assets/Airbnb-logo.png'
-import landingPageImg from './assets/landing-page-photo.png'
+import airBnb from '../public/assets/Airbnb-logo.png'
+import landingPageImg from '../public/assets/landing-page-photo.png'
 import Card from './components/Card.jsx'
 import './App.css'
-
+import data from "./components/Data.js"
 
 function MainContent() {
-  return (
-    <div className='hero'>
-        <img className='landing-page-img' src={landingPageImg}></img>
-        <h1 className='header-text'>Online Experience</h1>
-        <h2 className='normal-text'>Join unique interactive activities led by one-of-a-kind hosts--all without leaving home.</h2>
-    </div>
-  )
+    return (
+        <div className='hero'>
+            <img className='landing-page-img' src={landingPageImg}></img>
+            <h1 className='header-text'>Online Experience</h1>
+            <h2 className='normal-text'>Join unique interactive activities led by one-of-a-kind hosts--all without leaving home.</h2>
+        </div>
+    )
 }
 
 
 export default function App() {
-  return (
-    <div>
-      <nav>
-        <img src={airBnb}></img>
-      </nav>
-      <MainContent />
-      <Card />
-    </div>
-  )
+
+    const cards = data.map(item => {
+        return (
+            <Card key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.location}
+                price={item.price}
+            /> 
+        )
+    })
+    return (
+        <div>
+            <nav>
+                <img src={airBnb}></img>
+            </nav>
+            <MainContent />
+            {cards}
+
+        </div>
+    )
 }
--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -1,39 +1,20 @@
 import "./Card.css"
-import swimmerImg from "../assets/swimmer-img.png"
-import star from "../assets/star.png"
+import star from "../../public/assets/star.png"
 
-function SoldOutTag() {
+export default function Card(info) {
     return (
-        <h4 className='sold-out-text'>SOLD OUT</h4>
-    )
-}
-
-function CardInfo() {
-    return (
-        <div className="card-info">
-            <img className='start-img' src={star}></img>
-            <h4 className='rating'>5.0<span className='lighter-colour'>&nbsp;(6) .USA</span></h4>
-        </div>
-    )
-}
-
-function TextInfo() {
-    return (
-        <div className='block-info'>
-            <h4 className='font-one'>Life lessons with Kaite Zaferes</h4>
-            <h4 className='font-two'>From $138 <span className='font-three'>/ person</span></h4>
-        </div>
-    )
-}
-
-export default function Card() {
-    return (
-        <section>
-            <div>
-                <img className='container-swimmer-img' src={swimmerImg}></img>
-                <SoldOutTag />
-                <CardInfo />
-                <TextInfo />
+        <section className="info-row">
+            <div className="info-column">
+                <img className='container-swimmer-img' src={info.img}></img>
+                <h4 className='sold-out-text'>SOLD OUT</h4>
+                <div className="card-info">
+                    <img className='start-img' src={star}></img>
+                    <h4 className='rating'>{info.rating}<span className='lighter-colour'>&nbsp;{info.reviewCount} {info.location}</span></h4>
+                </div>
+                <div className='block-info'>
+                    <h4 className='font-one'>{info.title}</h4>
+                    <h4 className='font-two'>From {info.price} <span className='font-three'>/ person</span></h4>
+                </div>              
             </div>
         </section>
     )
